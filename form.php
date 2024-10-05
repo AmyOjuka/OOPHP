6249aa69220d5d8acdaef1af15ef6994a2e62d61
AmyOjuka <amy.ojuka@strathmore.edu>
2024-10-05 11:07:25 +0300

    js bootstrap

--- form.php
+++ form.php
@@ -70,5 +70,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         Password: <input type="password" name="password" required><br><br>
         <input type="submit" value="Register">    
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
