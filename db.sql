abd6da94f5b9fb5f2054a5317178dabd9b80a571
AmyOjuka <amy.ojuka@strathmore.edu>
2024-10-05 09:56:18 +0300

    Added primary Key

--- db.sql
+++ db.sql
@@ -4,11 +4,13 @@ USE `UserDB`;
 
 DROP TABLE IF EXISTS `gender`;
 CREATE TABLE IF NOT EXISTS `userDetails` (
+    `userId` tinyint(1) NOT NULL AUTO_INCREMENT,
     'Name' VARCHAR(255) NOT NULL,
     `gender` varchar(20) NOT NULL,
     'DateOfBirth' DATE NOT NULL,
     'Email' VARCHAR(255) NOT NULL UNIQUE,
     'Residence' VARCHAR(255) NOT NULL,
     'RegistrationDate' TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    'Password' VARCHAR(255) NOT NULL
+    'Password' VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`userId`)
 );
