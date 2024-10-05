3938e549f992156fac7d141cae9a968360c217ee
AmyOjuka <amy.ojuka@strathmore.edu>
2024-10-05 10:47:43 +0300

    form variables

--- form.php
+++ form.php
@@ -6,6 +6,11 @@ function sanitizeInput($data) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
+    $fullname = sanitizeInput($_POST['fullname']);
+    $gender = sanitizeInput($_POST['gender']);
+    $dob = sanitizeInput($_POST['dob']);
+    $email = sanitizeInput($_POST['email']);
+    $residence = sanitizeInput($_POST['residence']);
+    $password = sanitizeInput($_POST['password']);
 
 }
